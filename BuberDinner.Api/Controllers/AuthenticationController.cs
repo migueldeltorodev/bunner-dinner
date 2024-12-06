1aaea8f923f56ead0fc9240cf0e3f26b97e383bf
Miguel David González del Toro <migueldgdt@estudiantes.uci.cu>
2024-12-06 16:38:59 -0500

    build (AuthController): created basic auth controller

---
+++ BuberDinner.Api/Controllers/AuthenticationController.cs
@@ -0,0 +1,54 @@
+﻿using BuberDinner.Application.Services.Authentication;
+using BuberDinner.Contracts.Authentication;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BuberDinner.Api.Controllers
+{
+    [Route("auth")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly IAuthenticationService _authenticationService;
+
+        public AuthenticationController(IAuthenticationService authenticationService)
+        {
+            _authenticationService = authenticationService;
+        }
+
+        [HttpPost("register")]
+        public IActionResult Register(RegisterRequest request)
+        {
+            var registerResult = _authenticationService.Register(
+                request.FirstName,
+                request.LastName,
+                request.Email,
+                request.Password);
+
+            var response = new AuthenticationResponse(
+                registerResult.Id,
+                registerResult.FirstName,
+                registerResult.LastName,
+                registerResult.Email,
+                registerResult.Token);
+
+            return Ok(response);
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login(LoginRequest request)
+        {
+            var registerResult = _authenticationService.Login(
+                request.Email,
+                request.Password);
+
+            var response = new AuthenticationResponse(
+                registerResult.Id,
+                registerResult.FirstName,
+                registerResult.LastName,
+                registerResult.Email,
+                registerResult.Token);
+
+            return Ok(response);
+        }
+    }
+}
