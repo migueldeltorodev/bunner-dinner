76f4cc59449b60fcc41f45bfcf2f2ce99a306877
Miguel David González del Toro <migueldgdt@gmail.com>
2024-12-11 17:00:47 -0500

    refactor (User): make all properties init-only

--- BuberDinner.Domain/Entities/User.cs
+++ BuberDinner.Domain/Entities/User.cs
@@ -3,9 +3,9 @@
     public class User
     {
         public Guid Id { get; set; } = Guid.NewGuid();
-        public string FirstName { get; set; } = default!;
-        public string LastName { get; set; } = default!;
-        public string Email { get; set; } = default!;
-        public string Password { get; set; } = default!;
+        public string FirstName { get; init; } = default!;
+        public string LastName { get; init; } = default!;
+        public string Email { get; init; } = default!;
+        public string Password { get; init; } = default!;
     }
 }
