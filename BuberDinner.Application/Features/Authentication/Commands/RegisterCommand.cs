d0830f76d9e62583f3c61e2f8a0283885b444c35
Miguel David González del Toro <migueldgdt@gmail.com>
2024-12-14 15:36:21 -0500

    build (MediatR): installed mediatR package and create Register feature

---
+++ BuberDinner.Application/Features/Authentication/Commands/RegisterCommand.cs
@@ -0,0 +1,8 @@
+using BuberDinner.Application.Services.Authentication.Common;
+using ErrorOr;
+using MediatR;
+
+namespace BuberDinner.Application.Features.Authentication.Commands;
+
+public record RegisterCommand(string FirstName, string LastName, string Email, string Password)
+    : IRequest<ErrorOr<AuthenticationResult>>;
---
+++ BuberDinner.Application/Features/Authentication/Commands/RegisterCommandHandler.cs
@@ -0,0 +1,45 @@
+using BuberDinner.Application.Common.Interfaces.Authentication;
+using BuberDinner.Application.Common.Interfaces.Persistence;
+using BuberDinner.Application.Services.Authentication.Common;
+using BuberDinner.Domain.Common.Errors;
+using BuberDinner.Domain.Entities;
+using ErrorOr;
+using MediatR;
+
+namespace BuberDinner.Application.Features.Authentication.Commands;
+
+public class RegisterCommandHandler :
+    IRequestHandler<RegisterCommand, ErrorOr<AuthenticationResult>>
+{
+    private readonly IJwtTokenGenerator _jwtTokenGenerator;
+    private readonly IUserRepository _userRepository;
+
+    public RegisterCommandHandler(IJwtTokenGenerator jwtTokenGenerator, IUserRepository userRepository)
+    {
+        _jwtTokenGenerator = jwtTokenGenerator;
+        _userRepository = userRepository;
+    }
+
+    public async Task<ErrorOr<AuthenticationResult>> Handle(RegisterCommand request,
+        CancellationToken cancellationToken)
+    {
+        // Validate the user already exists
+        if (_userRepository.GetUserByEmail(request.Email) is not null) return Errors.User.DuplicateEmail;
+
+        // Create the user
+        var user = new User()
+        {
+            FirstName = request.FirstName,
+            LastName = request.LastName,
+            Email = request.Email,
+            Password = request.Password
+        };
+
+        _userRepository.Add(user);
+
+        // Create the JWT Token
+        var token = _jwtTokenGenerator.GenerateToken(user);
+
+        return new AuthenticationResult(user, token);
+    }
+}
