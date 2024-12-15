5aa35ba138a75981b4f9dae1f31d3455c62c25b0
Miguel David González del Toro <migueldgdt@gmail.com>
2024-12-15 15:08:42 -0500

    build (AuthMappingProfile): created Mapster profile

---
+++ BuberDinner.Api/Common/Mapping/AuthenticationMappingConfig.cs
@@ -0,0 +1,21 @@
+using BuberDinner.Application.Features.Authentication.Commands;
+using BuberDinner.Application.Features.Authentication.Common;
+using BuberDinner.Application.Features.Authentication.Queries;
+using BuberDinner.Contracts.Authentication;
+using Mapster;
+
+namespace BuberDinner.Api.Common.Mapping;
+
+public class AuthenticationMappingConfig : IRegister
+{
+    public void Register(TypeAdapterConfig config)
+    {
+        config.NewConfig<RegisterRequest, RegisterCommand>();
+
+        config.NewConfig<LoginRequest, LoginQuery>();
+
+        config.NewConfig<AuthenticationResult, AuthenticationResponse>()
+            .Map(dest => dest.Token, src => src.Token)
+            .Map(dest => dest, src => src.User);
+    }
+}
