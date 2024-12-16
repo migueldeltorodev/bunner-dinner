f8024e98bfcecd38b254b5949345a99e481bbbf1
Miguel David González del Toro <migueldgdt@gmail.com>
2024-12-16 17:49:46 -0500

    build (AuthValidators): created login and register records validators

---
+++ BuberDinner.Application/Features/Authentication/Queries/LoginQueryValidator.cs
@@ -0,0 +1,15 @@
+using FluentValidation;
+
+namespace BuberDinner.Application.Features.Authentication.Queries;
+
+public class LoginQueryValidator : AbstractValidator<LoginQuery>
+{
+    public LoginQueryValidator()
+    {
+        RuleFor(x => x.Email)
+            .NotEmpty()
+            .EmailAddress();
+        RuleFor(x => x.Password)
+            .NotEmpty();
+    }
+}
