1ed4d35900f3b1e5fc8060110376645b6592a8b0
Miguel David González del Toro <migueldgdt@gmail.com>
2024-12-24 16:52:53 -0500

    refactor (AuthController): using the [AllowAnonymous] atribute to access

--- BuberDinner.Api/Controllers/AuthenticationController.cs
+++ BuberDinner.Api/Controllers/AuthenticationController.cs
@@ -3,11 +3,13 @@ using BuberDinner.Application.Features.Authentication.Queries;
 using BuberDinner.Contracts.Authentication;
 using MapsterMapper;
 using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BuberDinner.Api.Controllers;
 
 [Route("auth")]
+[AllowAnonymous]
 public class AuthenticationController : ApiController
 {
     private readonly ISender _mediator;
