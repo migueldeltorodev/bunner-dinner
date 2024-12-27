452f108106abfdfef029772c1ba2d0af72dd7519
Miguel David González del Toro <migueldgdt@gmail.com>
2024-12-27 14:41:04 -0500

    build (ID): created ID value objects

---
+++ BuberDinner.Domain/Dinner/ValueObjects/DinnerId.cs
@@ -0,0 +1,24 @@
+using BuberDinner.Domain.Common.Models;
+using BuberDinner.Domain.Host.ValueObjects;
+
+namespace BuberDinner.Domain.Dinner.ValueObjects;
+
+public class DinnerId : ValueObject
+{
+    public Guid Value { get; }
+
+    private DinnerId(Guid value)
+    {
+        Value = value;
+    }
+
+    public static DinnerId CreateUnique()
+    {
+        return new DinnerId(Guid.NewGuid());
+    }
+
+    public override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return Value;
+    }
+}
---
+++ BuberDinner.Domain/Menu/ValueObjects/MenuSectionId.cs
@@ -0,0 +1,23 @@
+using BuberDinner.Domain.Common.Models;
+
+namespace BuberDinner.Domain.Menu.ValueObjects;
+
+public class MenuSectionId : ValueObject
+{
+    public Guid Value { get; }
+
+    private MenuSectionId(Guid value)
+    {
+        Value = value;
+    }
+
+    public static MenuSectionId CreateUnique()
+    {
+        return new MenuSectionId(Guid.NewGuid());
+    }
+
+    public override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return Value;
+    }
+}
